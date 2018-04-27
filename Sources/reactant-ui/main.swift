aed39385db2e22583e4fc010451b8b987e12db56
Matyáš Kříž <matyas@brightify.org>
2018-04-27 10:42:45 +0200

    Let the user know how to get more info about a certain command.

--- Sources/reactant-ui/main.swift
+++ Sources/reactant-ui/main.swift
@@ -9,7 +9,7 @@ let xsdCommand = XSDCommand()
 
 let cli = CLI(name: "generator",
               version: "0.3.0",
-              description: "Command line tool used to generate Swift UI code from XML UI",
+              description: "Command line tool used to generate Swift UI code from XML UI\nAdd `-h` after a command to see all its options.",
               commands: [generateCommand, xsdCommand])
 
 cli.goAndExit()
